36ba5e8ebc9be827524ea198e752d490a5fe616d
Gatovsky <101161255+hharieta@users.noreply.github.com>
2024-05-19 23:23:46 -0500

    First commit

---
+++ jobs/dockerweb/index.php
@@ -0,0 +1,14 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My PHP 8 Index Page</title>
+</head>
+<body>
+    <h1>Welcome to my PHP 8 Index Page</h1>
+    <?php
+    // PHP code here
+    echo "Hello, world!";
+    ?>
+</body>
+</html>
